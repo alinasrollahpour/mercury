6b2ec6c21cbc41ab95dd179ab41a3b6c336f4d1e
ali <alinalamdari@gmail.com>
2025-09-17 23:12:34 +0330

    it started working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import {useState} from 'react';
 
-async function openFileHandler() {
-  let res = await window.merc.selectVideoFile();
-  //res is undefined
-  console.log('$$$ selectVideoFile() has been invoked');
-  console.log(res);
-}
+
 
 export default function App() {
+
+  const [videoURL, setVideoURL] = useState();
+
+  async function openFileHandler() {
+    let res = await window.merc.selectVideoFile();
+    setVideoURL(res);
+
+    //res is undefined
+    console.log('$$$ selectVideoFile() has been invoked');
+    console.log(res);
+  }
+
   return <>
     <button id='open-file' onClick={openFileHandler}>Open a file</button>
-    <video controls width={300} height={200}/>
+    { videoURL &&
+      <video controls width={300} height={200} src={videoURL} />
+    }
   </>
 }
